e2e16aa11e9ad586afc3c708f8638c9fa1a032ff
Nichelle <nichelle.thinagar@gmail.com>
2025-07-18 08:52:17 -0400

    added some stuff

---
+++ tutorial_ardusub/PID_Stuff.py
@@ -0,0 +1,31 @@
+import sympy as sp
+import rclpy    # the ROS 2 client library for Python
+from rclpy.node import Node    # the ROS 2 Node class
+from rclpy.timer import Rate
+from sensor_msgs.msg import BatteryState, Imu
+from mavros_msgs.msg import OverrideRCIn, ManualControl
+import numpy as np
+import math
+import time
+
+
+KP = 0
+KI = 0
+KD = 0
+def maintain_depth(self, z, t):
+        self.get_logger().info("starting function")
+        start_time = self.get_clock().now()
+        while (self.get_clock().now() - start_time < rclpy.duration.Duration(seconds = t)) & rclpy.ok():
+            msg = ManualControl()
+            msg.x = x
+            msg.y = y
+            msg.z = z
+            msg.r = r
+            msg.t = t
+            self.pub.publish(msg)
+            # self.get_logger().info("Time: {:.2f}".format(self.get_clock().now() - start_time))
+            time.sleep(.05)
+        self.get_logger().info("ending function")
+error = target_zcurrent_z
+
+u = KP*error + KI*sp.integrate(error) + KD*sp.diff(error)
