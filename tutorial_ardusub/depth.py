8e0158bb47b42bf2b4b8280a3455a54ab26a279f
Nichelle-at <nichelle.thinagar@gmail.com>
2025-07-21 11:42:46 -0400

    fixing depth

--- tutorial_ardusub/depth.py
+++ tutorial_ardusub/depth.py
@@ -6,36 +6,6 @@ from std_msgs.msg import Int16, Float64
 from rclpy import timer
 
 
-class Current_Depth(Node):
-    def __init__(self):
-        super().__init__("bluerow_publisher")
-        self.sub = self.create_subscription(
-            FluidPressure,        
-            "/pressure",    
-            self.depth, 
-            10             
-        )
-        self.pub = self.create_publisher(
-            float64,        
-            "/depthNow",    
-            self.depth, 
-            10             
-        )
-    def calculate_depth(self, fp, d=1000, g=9.81, atmospheric_pressure=101.3250):
-        return -(fp - atmospheric_pressure) / (d * g)
-        
-def main1(args=None):
-        rclpy.init(args=args)
-        node = Current_Depth()
-
-        try:
-            rclpy.spin(node)
-        except KeyboardInterrupt:
-            print("\nKeyboardInterrupt received, shutting down...")
-        finally:
-            node.destroy_node()
-            if rclpy.ok():
-                rclpy.shutdown()
 
 class Z_axis_PID(Node):
     def __init__(self):
@@ -51,12 +21,6 @@ class Z_axis_PID(Node):
             self.depth, 
             10             
         )
-        self.create_subscription(
-            Float64,        
-            "/desired_depth",    
-            self.update_target_depth, 
-            10             
-        )
         self.Kp = 0
         self.Ki = 0
         self.Kd = 0
@@ -64,25 +28,18 @@ class Z_axis_PID(Node):
         self.error_accumulator = 0.0
         self.previous_error = 0.0
         self.timestep = 0.02
-<<<<<<< HEAD
-=======
-    def update_target_depth(self, msg):
-            self.target_depth = msg.data
-            self.get_logger().info(f"[TARGET SET] New target depth: {self.target_depth:.2f}")
-
     def calculate_depth(self, fp, d=1000, g=9.81, atmospheric_pressure=101325.0):
         return -(fp - atmospheric_pressure) / (d * g)
         
->>>>>>> dc5d2e329233bbfa0d66d8f8aba56438a3502860
     def depth(self, msg):
         self.get_logger().info(f"Descending to {self.target_depth}")
         current_depth = self.calculate_depth(msg.fluid_pressure)
         error = (self.target_depth - current_depth)
         self.error_accumulator += error * self.timestep
         derivative = (error - self.previous_error) / self.timestep
-        Kp = 20.0
-        Ki = 0.3
-        Kd = 2.0
+        Kp = 1.0
+        Ki = 0.01
+        Kd = 0.1
         integral = min(Ki * self.error_accumulator, 1.0)
         u = Kp * error + integral + Kd * derivative
         u = max(-100, min(u, 100))
